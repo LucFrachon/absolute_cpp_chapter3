6954ead1b5a11d04612422f64ba384a63db504fa
LucFrachon <luc.frachon@gmail.com>
2017-07-05 13:45:15 +0300

    style: Clean up code

--- game_of_craps.cpp
+++ game_of_craps.cpp
@@ -1,6 +1,8 @@
 /*
 Programming project 9: Game of Craps
 Simulate 10,000 games of craps and calculate win probability.
+In the long term, the player wins only 49.2% of rounds. Winning odds are therefore
+0.969:1. The bank wins in the long term!
 */
 
 #include <iostream>
@@ -9,7 +11,6 @@ using namespace std;
 
 const int SIDES = 6;  //Number of sides on the dice used in the game
 const int NUM_DICE = 2;  //Number of dice used in the game (identical dice)
-const int RAND_SEED = 23;  //Seed for pseudo-random generator (for reproducibility)
 
 int throw_dice();
 //Returns the sum of NUM_DICE throws of SIDES-sided dice using a pseudo-random generator.
@@ -26,7 +27,6 @@ bool first_throw_lose_condition(int score);
 //Precondition: Score is an integer.
 //Returns a boolean that indicates if the player lost on the first dice throw.
 
-
 int main()
 {
 	int num_rounds, win_counter = 0;  //How many games were won by player
@@ -36,11 +36,7 @@ int main()
 	cin >> num_rounds;
 
 	for (int i = 0; i < num_rounds; i++)  //Simulate as many games as requested by user
-	{
-		//cout << "\nRound " << i << endl;
 		win_counter += one_round();
-		//cout << "Rounds won so far: " << win_counter << endl;
-	}
 
 	//Display outcomes
 	cout << "Completed " << num_rounds << " rounds of craps.\n"
@@ -58,13 +54,8 @@ int main()
 int throw_dice()
 {
 	int score = 0;
-	//srand(RAND_SEED);
 	for (int i = 0; i < NUM_DICE; i++)
-	{
 		score += 1 + rand() % SIDES;  //Add up random throws of SIDE-sided dice
-		//cout << "Die " << i << " score " << score << endl;
-	}
-	//cout << "Score " << score << endl;
 	return score;
 }
 
@@ -85,21 +76,18 @@ int one_round()
 
 	//First throw
 	throw_result = throw_dice();
-	//cout << "First throw " << throw_result << endl;
 
 	if (first_throw_win_condition(throw_result))
 	{
 		win_lose = 1;
-		//cout << "WON on first throw" << endl;
 	}
 	else if (first_throw_lose_condition(throw_result))
 	{
 		win_lose = 0;
-		//cout << "LOST on first throw" << endl;
 	}
 	else
 	{
-		point = throw_result;  //The result become "the point"
+		point = throw_result;  //The result becomes "the point"
 		//The game continues
 	}
 
@@ -112,7 +100,6 @@ int one_round()
 		else if (throw_result == point)
 			win_lose = 1;
 	}
-	//cout << "Win / lose: " << win_lose << endl;
 
 	return win_lose;
 }
